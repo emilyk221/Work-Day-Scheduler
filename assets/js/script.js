63046fd2ff3bf8353cb3550a58ee3351010cb5df
emilyk <emily.g.kruse@gmail.com>
2022-03-06 16:33:50 -0500

    load stored events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 let events = [];
+
+// dispaly current day in the header
 let todayEl = $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+// create hour, textarea, and button columns within time-block rows
 for (let i = 9; i <= 17; i++) {
   let hourEl = $("<div>").addClass("col-md-1 hour")
 
@@ -22,47 +25,64 @@ for (let i = 9; i <= 17; i++) {
   hourRowEl.append(hourEl, textEl, saveEl);
 }
 
+// load appointments from localStorage
 let loadEvents = function() {
   let events = JSON.parse(localStorage.getItem("events"));
   if (!events) {
     events = [];
-    return false;
+    //return false;
   }
 
-  // get hour value from each object in events array
-  // match hour value from events array (storage) to time-block row
-  // append text value to textarea inside correct time-block row 
+  // get hour and text value from each object in events array
+  for (let i = 0; i < events.length; i++) {
+    let hour = events[i].hour;
+    let text = events[i].text;
+
+    // change text in textarea to stored value in corresponding time-block row
+    $("#hour-" + hour)
+      .find("textarea")
+      .text(text);
+  }
 }
 
+// save event appointments to local storage
 let saveEvents = function() {
   localStorage.setItem("events", JSON.stringify(events));
 }
 
+// hide notification of appointment saved
 let clearNotify = function() {
   setTimeout(function() {
     $("#notify").hide();
   }, 2000);
 }
 
+// when save button is clicked, get text from textarea and corresponding hour to put into localStorage
 $(".time-block").on("click", "button", function() {
+  // get value of text from textarea
   let text = $(this)
     .prev("textarea")
     .val();
 
+  // get the hour/time from the time-block row id
   let hourRow = $(this)
     .closest(".time-block")
     .attr("id")
     .replace("hour-", "");
 
-  tempArr = {
+  // put values in temp object
+  tempObj = {
     hour: hourRow,
     text: text
   };
   
-  events.push(tempArr);
+  // add temp object to events array being saved to localStorage 
+  events.push(tempObj);
   
+  // save events
   saveEvents();
-
+  
+  // show then hide notification of appointments being saved
   let notifyContainer = $("#notify");
   
   let saveNotify = $("<p>")
@@ -72,6 +92,7 @@ $(".time-block").on("click", "button", function() {
   clearNotify();
 })
 
+// check time and compare to time-blocks, add classes to change color based on time
 let checkTime = function(timeEl) {
   // get time from hour element
   let time = $(timeEl).find("div").text().trim();
@@ -85,7 +106,7 @@ let checkTime = function(timeEl) {
   // remove any old classes from element
   $(timeEl).removeClass("present future");
 
-  // apply new class if task is near/over due date
+  // apply new class if hour is current or in future
   if (moment().isBetween(time, withinHour)) {
     $(timeEl).addClass("present");
   }
@@ -94,12 +115,14 @@ let checkTime = function(timeEl) {
   }
 };
 
+// check time when page loads
 setTimeout(function() {
   $(".time-block").each(function(index, el) {
     checkTime(el);
   });
 }, 1);
 
+// check time every two minutes
 setInterval(function() {
   $(".time-block").each(function(index, el) {
     checkTime(el);
